c5620207847987bfbc273764e3f773000eecfee1
M. Raihan Pratama <rehan555pertama@gmail.com>
2026-09-15 16:06:08 +0700

    sapi

---
+++ main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("abjada");
+        Scanner scan = new Scanner(System.in);
+        while (true){
+        int a = scan.nextInt();
+        for (int i=1;i<=a;i++){
+            System.out.println(i);
+        }
+        if (a==-1)break;
+        }
+        
+    }
+
+    public static int sapi() {
+        return 45577;
+    }
+}
